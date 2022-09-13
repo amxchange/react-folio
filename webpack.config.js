abed14950d6f5be018c59b30b8c320917fc83f24
VJY <vj@LAYAMEXIM94.local>
2022-09-14 00:24:54 +0530

    optimisation

--- webpack.config.js
+++ webpack.config.js
@@ -147,7 +147,7 @@ module.exports = (env, options) => {
             new CleanWebpackPlugin(),
             new HtmlWebpackPlugin({
                 template: "./src/index.html",
-                base: isDevelopment ? "" : "https://amxchange.github.io/react-folio/", //TODO- make dynamic
+                base: !isDevelopment && env.ghPages === "true" ? "https://amxchange.github.io/react-folio/" : "", //TODO- can expect base url in args itself
                 filename: "index.html",
                 favicon: "./src/shared/styles/favicon.ico"
             }),
